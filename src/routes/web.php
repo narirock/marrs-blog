2124b77f5470f8682f74984119ebe3f72db2b3ec
narirock <nari9rock@hotmail.com>
2021-09-30 19:10:29 -0300

    permitindo não utilizar rotas padrão.

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Config;
 //'middleware' => 'webadmin',
 
 //verificando se rota é protegida;
@@ -10,18 +11,18 @@ if (config('marrs-blog.guard') != "") {
 }
 
 
-
-
-Route::group(
-    ['prefix' => 'admin/blog', 'middleware' => $guard],
-    function () {
-        Route::resource('categories', 'Marrs\MarrsBlog\Http\Controllers\Admin\CategoryController', ['as' => 'admin.blog']);
-        Route::resource('posts', 'Marrs\MarrsBlog\Http\Controllers\Admin\PostController', ['as' => 'admin.blog']);
-    }
-);
-
-
-Route::group(['prefix' => 'blog', 'middleware' => ['web']], function () {
-    Route::get('/', 'Marrs\MarrsBlog\Http\Controllers\Front\BlogController@index')->name('blog.index');
-    Route::get('/post/{slug}', 'Marrs\MarrsBlog\Http\Controllers\Front\BlogController@post')->name('blog.post');
-});
+if (!Config::get('marrs-blog.disable.admin.routes')) {
+    Route::group(
+        ['prefix' => 'admin/blog', 'middleware' => $guard],
+        function () {
+            Route::resource('categories', 'Marrs\MarrsBlog\Http\Controllers\Admin\CategoryController', ['as' => 'admin.blog']);
+            Route::resource('posts', 'Marrs\MarrsBlog\Http\Controllers\Admin\PostController', ['as' => 'admin.blog']);
+        }
+    );
+}
+if (!Config::get('marrs-blog.disable.frontend.routes')) {
+    Route::group(['prefix' => 'blog', 'middleware' => ['web']], function () {
+        Route::get('/', 'Marrs\MarrsBlog\Http\Controllers\Front\BlogController@index')->name('blog.index');
+        Route::get('/post/{slug}', 'Marrs\MarrsBlog\Http\Controllers\Front\BlogController@post')->name('blog.post');
+    });
+}
